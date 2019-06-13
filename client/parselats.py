9589f3fef6101a5d048b64d431b7dce92cae1027
Kostis Kaffes <kkaffes@stanford.edu>
2019-06-12 20:21:05 -0400

    Update parselats.py to Python3

--- client/parselats.py
+++ client/parselats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import os
@@ -10,7 +10,7 @@ class Lat(object):
     def __init__(self, fileName):
         f = open(fileName, 'rb')
         a = np.fromfile(f, dtype=np.uint64)
-        self.reqTimes = a.reshape((a.shape[0]/1, 1))
+        self.reqTimes = a.reshape((a.shape[0], 1))
         f.close()
 
     def parseQueueTimes(self):
@@ -32,19 +32,19 @@ if __name__ == '__main__':
         # svcTimes = [l/1e6 for l in latsObj.parseSvcTimes()]
         sjrnTimes = [l/1e3 for l in latsObj.parseSojournTimes()]
 
-        print len(sjrnTimes)
+        print(len(sjrnTimes))
         mean = np.mean(sjrnTimes)
-        print mean
+        print(mean)
         median = stats.scoreatpercentile(sjrnTimes, 50)
-        print median
+        print(median)
         p95 = stats.scoreatpercentile(sjrnTimes, 95)
-        print p95
+        print(p95)
         p99 = stats.scoreatpercentile(sjrnTimes, 99)
-        print p99
+        print (p99)
         p999 = stats.scoreatpercentile(sjrnTimes, 99.9)
-        print p999
+        print (p999)
         maxLat = max(sjrnTimes)
-        print maxLat
+        print(maxLat)
 
     latsFile = sys.argv[1]
     getLatPct(latsFile)
