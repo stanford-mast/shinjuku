dd0103296156ddead281edda12756851cb629854
Kostis Kaffes <kkaffes@stanford.edu>
2022-07-10 14:13:21 -0700

    Increase the preemption delay even more

--- dp/core/dispatcher.c
+++ dp/core/dispatcher.c
@@ -34,7 +34,7 @@
 extern void dune_apic_send_posted_ipi(uint8_t vector, uint32_t dest_core);
 
 #define PREEMPT_VECTOR 0xf2
-#define PREEMPTION_DELAY 5000000
+#define PREEMPTION_DELAY 500000000000000
 
 static void timestamp_init(int num_workers)
 {
