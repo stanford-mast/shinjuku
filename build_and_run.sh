c11e442debbb8e4f543045e19044b1910426d571
Kostis Kaffes <kkaffes@stanford.edu>
2022-07-14 11:35:45 -0700

    Fix frequency

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-
+set -e
+set -x
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 make clean
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+echo 1700000 | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+echo 1700000 | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
 make -sj64
+echo "" > output.txt
 ./dp/shinjuku
